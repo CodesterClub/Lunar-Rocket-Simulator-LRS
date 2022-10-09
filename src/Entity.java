ebc1dff5d92605d9a6a13727452c504d7960bce3
AvirukBasak <avirukbasak@gmail.com>
2022-10-09 18:58:20 +0530

    updated docs for System.java

--- src/Entity.java
+++ src/Entity.java
@@ -10,7 +10,7 @@ public class Entity
     boolean isSoft; // a soft entity cannot interact with any other entity
     protected BufferedImage entityImg;
     protected List<Entity> attatchments;
-    public Entity(int x, int y, BufferedImage img)
+    public Entity(BufferedImage img, int x, int y)
     {
         this.isSoft = false;
         this.x = x;
@@ -19,7 +19,7 @@ public class Entity
         else throw new NullPointerException("Entity: image is null");
         this.xBound = img.getWidth();
         this.yBound = img.getHeight();
-        this.attatchments = new ArrayList<Entity>();
+        this.attatchments = null;
     }
     public void update(int dx, int dy)
     {
@@ -30,17 +30,35 @@ public class Entity
             e.y += dy;
         }
     }
+    public void move(int x, int y)
+    {
+        int dx = x - this.x;
+        int dy = y - this.y;
+        this.update(dx, dy);
+    }
+    /** Draw this and its attatchments */
     public void render(Graphics gfx)
     {
         gfx.drawImage(this.entityImg, this.x, this.y, null);
+        for (Entity e : this.attatchments)
+            gfx.drawImage(e.entityImg, e.x, e.y, null);
     }
+    /**
+     * Attatch multiple entities to create a larger compound
+     * Do note that newly attatched entities get drawn over older ones
+     * Also, the parent entity is drawn before its attatchments
+     */
     public void attatch(Entity e)
     {
+        if (this.attatchments == null)
+            this.attatchments = new ArrayList<Entity>();
         this.attatchments.add(e);
     }
     public void flush()
     {
         this.entityImg.flush();
         this.entityImg = null;
+        for (Entity e : this.attatchments)
+            e.flush();
     }
 }
--- src/System.java
+++ src/System.java
@@ -8,33 +8,62 @@ import java.util.ArrayList;
 
 public class System
 {
+    protected int width, height;
     protected BufferedImage bg;
-    protected List<Entity> entities;
-    protected Graphics gfx;
-    protected Canvas cv;
     protected BufferStrategy bs;
 
     protected int x, y;
+    protected List<Entity> entities;
 
+    /**
+     * A system is a collection of many entites
+     * @param Canvas The canvas where the system is drawn
+     * @param BufferedImage The background image
+     * @param int x coordinate of system bg
+     * @param int y coordinate of system bg
+     */
     public System(Canvas canvas, BufferedImage bg, int x, int y)
     {
-        this.cv = canvas;
+        this.bg = bg;
+        this.x = x;
+        this.y = y;
+        this.width = canvas.getWidth();
+        this.height = canvas.getHeight();
         this.bs = canvas.getBufferStrategy();
         if (this.bs == null) {
             canvas.createBufferStrategy(3);
             this.bs = canvas.getBufferStrategy();
         }
-        this.gfx = bs.getDrawGraphics();
-        Toolkit.getDefaultToolkit().sync();
-        this.bg = bg;
-        this.x = x;
-        this.y = y;
+        this.entities = null;
     }
+    /**
+     * add an entity to the system
+     * @param Entity
+     */
+    public void addEntity(Entity e)
+    {
+        if (this.entities == null)
+            this.entities = new ArrayList<Entity>();
+        this.entities.add(e);
+    }
+    /**
+     * update the system, keeps entities static
+     * useful for motion in POV of entities
+     * @param int delta-x
+     * @param int delta-y
+     */
     public void update(int dx, int dy)
     {
         this.x += dx;
         this.y += dy;
     }
+    /**
+     * update the system with entites
+     * useful for motion in POV of player
+     * @param int delta-x
+     * @param int delta-y
+     * @param boolean movEntities
+     */
     public void update(int dx, int dy, boolean movEntities)
     {
         this.x += dx;
@@ -43,15 +72,50 @@ public class System
             for (Entity e : this.entites)
                 e.update(dx, dy);
     }
+    /**
+     * update the system by absolute position
+     * @param int new-x
+     * @param int new-y
+     */
+    public void move(int x, int y)
+    {
+        int dx = x - this.x;
+        int dy = y - this.y;
+        this.update(dx, dy);
+    }
+    /**
+     * update the system with enties by absolute position
+     * @param int new-x
+     * @param int new-y
+     * @param boolean movEntities
+     */
+    public void move(int x, int y, boolean movEntities)
+    {
+        int dx = x - this.x;
+        int dy = y - this.y;
+        this.update(dx, dy, true);
+    }
+    /**
+     * render system with the entities
+     */
     public void render()
     {
-        gfx.clearRect(0, 0, this.cv.getWidth(), this.cv.getHeight());
+        Graphics gfx = this.bs.getDrawGraphics();
+        gfx.clearRect(0, 0, this.width, this.height);
         gfx.drawImage(this.bg, this.x, this.y, null);
         for (Entity e : this.entites)
             e.render(gfx);
+        this.bs.show();
+        Toolkit.getDefaultToolkit().sync();
+        gfx.dispose();
     }
+    /**
+     * flush system with bg and all resources of its entities
+     */
     public void flush()
     {
-        bg.flush;
+        this.bg.flush();
+        for (Entity e : this.entities)
+            e.flush();
     }
 }
